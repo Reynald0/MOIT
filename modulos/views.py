db841b6e0970e92742d3fd6dc23988ff36dc9fc8
Reynald0 <reynaldobernard15@gmail.com>
2016-07-16 04:22:07 -0500

    Zona horaria UTC-5

--- modulos/views.py
+++ modulos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.utils import timezone
+import datetime
 from .models import Modulo
 
 def inicio(request):
-	fecha_actual = timezone.now()
+	fecha_actual = datetime.datetime.now() - datetime.timedelta(hours=5)
 	modulos = Modulo.objects.all()
 	return render(request, 'inicio.html', {'fecha_actual': fecha_actual, 'modulos': modulos })
 
